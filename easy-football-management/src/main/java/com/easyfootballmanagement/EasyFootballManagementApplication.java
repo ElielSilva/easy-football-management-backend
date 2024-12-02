6e1979f0f4c02cff8c1d3487bf4ec21f7e2f4df2
Jadeilson Martiho <jadeilsonm17@gmail.com>
2024-12-02 19:06:17 -0300

    chore: inital structure

---
+++ easy-football-management/src/main/java/com/easyfootballmanagement/EasyFootballManagementApplication.java
@@ -0,0 +1,13 @@
+package com.easyfootballmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyFootballManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyFootballManagementApplication.class, args);
+    }
+
+}
---
+++ easy-football-management/src/test/java/com/easyfootballmanagement/EasyFootballManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyfootballmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyFootballManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
