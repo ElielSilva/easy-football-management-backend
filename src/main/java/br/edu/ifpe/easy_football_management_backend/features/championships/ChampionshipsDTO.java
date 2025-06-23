788763f062e5eda61bd79e656bc4806df5c5ebee
jadeilsonm <jadeilsonm17@gmail.com>
2025-06-23 18:39:09 -0300

    wip

--- src/main/java/br/edu/ifpe/easy_football_management_backend/features/championships/ChampionshipsDTO.java
+++ src/main/java/br/edu/ifpe/easy_football_management_backend/features/championships/ChampionshipsDTO.java
@@ -2,6 +2,8 @@ package br.edu.ifpe.easy_football_management_backend.features.championships;
 
 import br.edu.ifpe.easy_football_management_backend.domain.entity.StatusChampionship;
 import br.edu.ifpe.easy_football_management_backend.domain.entity.TypeChampionship;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -19,5 +21,7 @@ public record ChampionshipsDTO(@NotNull
                                @NotNull
                                TypeChampionship typeChampionship,
                                UUID userID,
-                               float award) {
+                               @DecimalMax(value = "9999999999.99", message = "O prêmio não pode ser maior que 9999999999.99")
+                               @DecimalMin(value = "0.01", message = "O prêmio deve ser pelo menos 0.01")
+                               double award) {
 }
